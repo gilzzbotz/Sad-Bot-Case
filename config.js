6752db0036d411bf1fbb4640bd6bb659fcf4d0f3
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-12 08:19:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6282287219167']
-global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
+global.owner = ['6285730794089']
+global.ownernomer = "6285730794089"
+global.premium = ['6285730794089']
 global.packname = 'Sticker By'
-global.author = 'Sad-Bot'
+global.author = 'Gilzz Bot'
 global.sessionName = 'papah'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -59,15 +59,15 @@ global.thumb = fs.readFileSync('./media/image/papah.jpg')
 global.faall = fs.readFileSync('./media/image/menu.jpg')
 
 // Url
-global.mygit = 'https://github.com/FahriAdison'
-global.myyt = 'https://bit.ly/Papah-Chan'
+global.mygit = 'https://github.com/gilzzattack'
+global.myyt = 'https://youtube.com/channel/UCPH9AdS6UQImcRv6IFtQ5lg'
 global.myytv = ''
-global.mygc = ""
+global.mygc = "https://chat.whatsapp.com/CxJShadfxYqCVAHHxdHSiy"
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'SAD BOT'
-global.akulaku = 'Bot By Papah'
-global.ytname = 'YT PAPAH' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = 'GILZZ BOT'
+global.akulaku = 'Bot By Gilzz'
+global.ytname = 'Gilzz Botz' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
